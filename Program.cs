2aa64ed1efe31ca3dda30ab0cee0078ec1557ea1
DESKTOP-T7PH5A9\nada <nadahassanin189@gmail.com>
2026-04-10 20:20:41 +0200

    QUESTION02

--- Program.cs
+++ Program.cs
@@ -29,6 +29,31 @@ namespace ConsoleApp9
             //watch.Stop();
             //Console.WriteLine("Time: " + watch.ElapsedMilliseconds);
             #endregion
+            #region Question 02
+            
+            //Console.Write("Age: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Day (6=Fri, 7=Sat): ");
+            //int day = int.Parse(Console.ReadLine());
+            //Console.Write("Student (yes/no): ");
+            //string student = Console.ReadLine();
+
+            //double price = 0;
+
+            //if (age < 5) price = 0;
+            //else if (age <= 12) price = 30;
+            //else if (age <= 59) price = 50;
+            //else price = 25;
+
+            
+            //if (price > 0 && (day == 6 || day == 7)) price = price + 10;
+
+            
+            //if (student == "yes") price = price * 0.8;
+
+            //Console.WriteLine("Final Price: " + price);
+            #endregion
+
 
         }
     }
