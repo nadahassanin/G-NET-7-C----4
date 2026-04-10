51f948cd29297ec6583df6d18861b9bf4291bcc2
DESKTOP-T7PH5A9\nada <nadahassanin189@gmail.com>
2026-04-10 20:13:06 +0200

    QUESTION01

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
+            #region Question 01
+            // (a) Why is it bad?
+            // Because the computer creates a new string every time we add a number. 
+            // This makes the memory very busy and slow.
+
+            // (b) Simple StringBuilder:
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append(i + ",");
+            //}
+            //Console.WriteLine(sb.ToString());
+
+            // (c) Timing (The easiest way):
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+            // ... put code here ...
+            //watch.Stop();
+            //Console.WriteLine("Time: " + watch.ElapsedMilliseconds);
+            #endregion
 
         }
     }
